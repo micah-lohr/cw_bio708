d533ba4942defdc17686a1d05f2a8473b4124078
Micah Lohr <mflohr@uncg.edu>
2026-09-17 10:31:42 -0400

    added different sample_n sizes to sampling lab

--- code/stats04_sampling_lab.R
+++ code/stats04_sampling_lab.R
@@ -7,35 +7,56 @@ df_h0 <- read_csv("data_src/data_plant_height.csv")
 
 true_mu_i <- mean(df_h0$height)
 true_var_ub_i <- var(df_h0$height)
-
-mu_i <- var_ub_i <- NULL
-
+# sample size 50
+mu_50 <- var_ub_50 <- NULL
 for(i in 1:100) {
-  df_i <- df_h0 %>% 
+  df_50 <- df_h0 %>% 
     sample_n(size = 50)
-  
-  ## Mean for a subset
-  mu_i[i] <- mean(df_i$height)
-  
-  ## Variance for a subset
-  var_ub_i[i] <- var(df_i$height)
+  mu_50[i] <- mean(df_50$height)
+  var_ub_50[i] <- var(df_50$height)
 }
 
-df_sample <- tibble(mu_hat = mu_i, var_ub_hat = var_ub_i)
+#sample size 100
+mu_100 <- var_ub_100 <- NULL
+for(i in 1:100) {
+  df_100 <- df_h0 %>% 
+    sample_n(size = 100)
+  mu_100[i] <- mean(df_100$height)
+  var_ub_100[i] <- var(df_100$height)
+}
 
-g_mu <- df_sample %>% 
-  ggplot(aes(x=mu_hat)) + 
+#graphing
+df_sample <- tibble(mu_hat50 = mu_50, var_ub_hat50 = var_ub_50, mu_hat100=mu_100, var_ub_hat100 = var_ub_100)
+
+g_mu50 <- df_sample %>% 
+  ggplot(aes(x=mu_hat50)) + 
   geom_histogram() +
   geom_vline(xintercept=true_mu_i) +
   theme_classic()
 
-g_var_ub <- df_sample %>% 
-  ggplot(aes(x=var_ub_hat)) +
+g_var_ub50 <- df_sample %>% 
+  ggplot(aes(x=var_ub_hat50)) +
   geom_histogram() +
   geom_vline(xintercept=true_var_ub_i) +
   theme_classic()
 
-g_mu / g_var_ub
+g_mu50 / g_var_ub50
+
+g_mu100 <- df_sample %>% 
+  ggplot(aes(x=mu_hat100)) + 
+  geom_histogram() +
+  geom_vline(xintercept=true_mu_i) +
+  theme_classic()
+
+g_var_ub100 <- df_sample %>% 
+  ggplot(aes(x=var_ub_hat100)) +
+  geom_histogram() +
+  geom_vline(xintercept=true_var_ub_i) +
+  theme_classic()
+
+g_mu100 / g_var_ub100
+
+(g_mu50 / g_var_ub50) | (g_mu100 / g_var_ub100)
 
 # (2) Sample means and unbiased variances are unbiased if samples are randomly selected. 
 # What happens if samples are non-random? Suppose the investigator was unable to find plants less than 10 cm in height – 
@@ -45,39 +66,63 @@ df_h10 <- df_h0 %>%
   filter(height >= 10)
 
 # Repeat step 1 with df_h10 instead of df_h0 and compare the results.
-
 true_mu_i2 <- mean(df_h10$height)
 true_var_ub_i2 <- var(df_h10$height)
 
-mu_i2 <- var_ub_i2 <- NULL
-
+#sample size 50
+mu_50_2 <- var_ub50_2 <- NULL
 for(i in 1:100) {
-  df_i2 <- df_h10 %>% 
+  df_50_2 <- df_h10 %>% 
     sample_n(size = 50)
-  
-  ## Mean for a subset
-  mu_i2[i] <- mean(df_i2$height)
-  
-  ## Variance for a subset
-  var_ub_i2[i] <- var(df_i2$height)
+  mu_50_2[i] <- mean(df_50_2$height)
+  var_ub50_2[i] <- var(df_50_2$height)
+}
+#sample size 50
+mu_100_2 <- var_ub100_2 <- NULL
+for(i in 1:100) {
+  df_100_2 <- df_h10 %>% 
+    sample_n(size = 100)
+  mu_100_2[i] <- mean(df_100_2$height)
+  var_ub100_2[i] <- var(df_100_2$height)
 }
 
-df_sample2 <- tibble(mu_hat = mu_i2, var_ub_hat = var_ub_i2)
 
-g_mu2 <- df_sample2 %>% 
-  ggplot(aes(x=mu_hat)) + 
+df_sample2 <- tibble(mu_hat50 = mu_50_2, var_ub_hat50 = var_ub50_2, mu_hat100 = mu_100_2, var_ub_hat100 = var_ub100_2)
+
+g_mu50_2 <- df_sample2 %>% 
+  ggplot(aes(x=mu_hat50)) + 
   geom_histogram() +
   geom_vline(xintercept=true_mu_i2) +
   theme_classic()
 
-g_var_ub2 <- df_sample2 %>% 
-  ggplot(aes(x=var_ub_hat)) +
+g_var_ub50_2 <- df_sample2 %>% 
+  ggplot(aes(x=var_ub_hat50)) +
   geom_histogram() +
   geom_vline(xintercept=true_var_ub_i2) +
   theme_classic()
 
-g_mu2 / g_var_ub2
+g_mu50_2 / g_var_ub50_2
 
-(g_mu / g_var_ub) + (g_mu2 / g_var_ub2)
+g_mu100_2 <- df_sample2 %>% 
+  ggplot(aes(x=mu_hat100)) + 
+  geom_histogram() +
+  geom_vline(xintercept=true_mu_i2) +
+  theme_classic()
+
+g_var_ub100_2 <- df_sample2 %>% 
+  ggplot(aes(x=var_ub_hat100)) +
+  geom_histogram() +
+  geom_vline(xintercept=true_var_ub_i2) +
+  theme_classic()
+
+g_mu100_2 / g_var_ub100_2
 
 # When samples are non-random, there is less skew in the distributions of the samples, and they appear to be clustered closer to the true population
+
+# (3) Make a dataframe with all four using pivot_long
+
+df_long <- pivot_longer(df_sample, cols= everything(), names_to="type", values_to="value")
+
+df_long %>% ggplot(aes(x=value)) +
+  geom_histogram() +
+  facet_wrap(~type) 
